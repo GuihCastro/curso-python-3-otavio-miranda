1fefebdf74adabddf8fdf13299de4a9bb0bf5cd5
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-12-17 22:30:16 -0300

    add aula113 sobre Polimorfismo

---
+++ aulas/aula113.py
@@ -0,0 +1,10 @@
+"""
+Polimorfismo em Python Orientado a Objetos
+
+Polimorfismo é o princípio que permite que classes deridavas de uma mesma superclasse tenham métodos iguais (com mesma assinatura) mas comportamentos diferentes.
+    -> Assinatura do método = Mesmo nome e quantidade de parâmetros
+
+SO|->L<-|ID
+Princípio da substituição de Liskov
+    -> Objetos de uma superclasse devem ser substituíveis por objetos de uma subclasse sem quebrar a aplicação.
+"""
